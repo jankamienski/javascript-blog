32c43d4b6d176c18e5571d1526466f414000ce27
jankamienski <kamienski.jan@gmail.com>
2020-10-28 15:52:07 +0100

    Add Handlebars to Eslint, Add templates to tag cloud, arrticles, tags below article, author below article active title. Author - right list still for fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-
-
 'use strict';
+
+const templates = {
+  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
+  tagLink: Handlebars.compile(document.querySelector('#template-tag-link').innerHTML),
+  authorLink: Handlebars.compile(document.querySelector('#template-author-link').innerHTML),
+  tagCloudLink: Handlebars.compile(document.querySelector('#template-tag-cloud-link').innerHTML)
+};
 
 const titleClickHandler = function(event){
   event.preventDefault();   
@@ -79,7 +84,9 @@ function generateTitleLinks(customSelector = ''){
 
   
     /* [DONE] create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    //const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTMLData = {id: articleId, title: articleTitle};
+    const linkHTML = templates.tagLink(linkHTMLData);
     console.log(linkHTML);
     
     /* [DONE] insert link into titleList */
@@ -158,11 +165,12 @@ function generateTags(){
       console.log(tag);
 
       /* [DONE] generate HTML of the link */
-      const tagHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-      console.log(tagHTML);
+      //const tagHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      const linkHTMLData = {tag: tag};
+      const linkHTML = templates.tagLink(linkHTMLData);
     
       /* [DONE] add generated code to html variable */
-      html = html + tagHTML;
+      html = html + linkHTML;
       console.log(html);
 
 
@@ -196,26 +204,31 @@ function generateTags(){
   console.log('tagsParams:', tagsParams);
 
   /* [NEW] create variable for all links HTML code */
-  let allTagsHTML = '';
-
+  //let allTagsHTML = '';
+  const allTagsData = {tags: []};
   /* [NEW] START LOOP: for each tag in allTags: */
   for(let tag in allTags){
   /* [NEW] generate code of a link and add it to allTagsHTML */
     //allTagsHTML += tag + ' (' + allTags[tag] + ') ';
-  
+    allTagsData.tags.push({
+      tag: tag,
+      count: allTags[tag],
+      className: calculateTagClass(allTags[tag], tagsParams)
+    });
     //const tagLinkHTML = '<li>' + calculateTagClass(allTags[tag], tagsParams) + '</li>';
-    allTagsHTML += '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">' + tag + '</a></li>';
+    //allTagsHTML += '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">' + tag + '</a></li>';
     //<li><a href="#">code</a> <span>(5)</span></li>
-    console.log('allTagsHTML:', allTagsHTML);
-    console.log(allTags[tag], tagsParams);
+    //console.log('allTagsHTML:', allTagsHTML);
+    //console.log(allTags[tag], tagsParams);
    
 
   /* [NEW] END LOOP: for each tag in allTags: */
   }
 
   /*[NEW] add HTML from allTagsHTML to tagList */
-  tagList.innerHTML = allTagsHTML;
-
+  //tagList.innerHTML = allTagsHTML;
+  tagList.innerHTML = templates.tagCloudLink(allTagsData);
+  console.log(allTagsData);
 }
 
 generateTags();
@@ -346,7 +359,9 @@ function generateAuthors() {
     const authorHTML ='<a href="#author-' + articleAuthors + '">' + articleAuthors + '</a>';
     console.log(authorHTML);
         
-
+    const linkHTMLData = {author: articleAuthors};
+    const linkHTML = templates.authorLink(linkHTMLData);
+   
     /* [NEW] check if this link is NOT already in allAuthors */
     //if(allAuthors.indexOf(authorHTML) == -1){
     /* [NEW] add generated code to allTags array */
@@ -361,7 +376,7 @@ function generateAuthors() {
          
     /* [DONE] insert HTML of all the links into the tags wrapper */
     //authorList.innerHTML = html;
-    authorList.innerHTML = authorHTML;
+    authorList.innerHTML = linkHTML;
     /* [DONE] END LOOP: for every article: */
   }
 
